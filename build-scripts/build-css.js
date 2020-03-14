461d32b193c06623748eeb233e8ecad49542de4d
Wessel Smit <wrsmit00@gmail.com>
2020-03-14 17:49:01 +0100

    css minify + rename

--- build-scripts/build-css.js
+++ build-scripts/build-css.js
@@ -3,9 +3,13 @@ const {
 	dest
 } = require('gulp')
 const cssImport = require('gulp-cssimport')
+const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename')
 
 return src([__dirname + '/../static/styles/main.css'])
 	.pipe(cssImport())
+	.pipe(cleanCSS({
+		compatibility: 'ie8'
+	}))
 	.pipe(rename('bundled.css'))
 	.pipe(dest(__dirname + '/../static/public/'))
