2b164c5628ea7330ee17d70253f40225f4efe257
Wessel Smit <wrsmit00@gmail.com>
2020-03-17 15:17:39 +0100

    added comments

--- static/serviceWorker1.js
+++ static/serviceWorker1.js
@@ -1,7 +1,7 @@
 const cacheName = 'cache-v1'
 
 self.addEventListener('install', e => {
-	self.skipWaiting()
+	self.skipWaiting() //Use newer version in waiting state if available
 })
 
 self.addEventListener('activate', e => {
@@ -23,8 +23,9 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
 	e.respondWith(
 		fetch(e.request)
+		//If a network connection is available => use fetched data || if not => use the cached version
 		.then(res => {
-			const resClone = res.clone()
+			const resClone = res.clone() //Clone response
 			caches
 				.open(cacheName)
 				.then(cache => cache.put(e.request, resClone))
