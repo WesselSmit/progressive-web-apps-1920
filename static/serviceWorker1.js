16716e49b0124a3ffdc8acff64aff46d6b64a360
Wessel Smit <wrsmit00@gmail.com>
2020-03-17 14:51:51 +0100

    removed console.logs

--- static/serviceWorker1.js
+++ static/serviceWorker1.js
@@ -1,21 +1,16 @@
-const cacheName = 'dynamic'
+const cacheName = 'cache-v1'
 
 self.addEventListener('install', e => {
-	console.log("serviceWorker installed!")
-
 	self.skipWaiting()
 })
 
 self.addEventListener('activate', e => {
-	console.log("serviceWorker activated!")
-
 	e.waitUntil(
 		clients.claim() //Sync serviceWorkers across all active clients
 		.then(() => caches.keys() //Remove outdated caches
 			.then(cacheNames => {
 				return Promise.all(
 					cacheNames.map(cache => {
-						console.log('clearing old caches!')
 						if (cache !== cacheName) {
 							return caches.delete(cache)
 						}
@@ -26,8 +21,6 @@ self.addEventListener('activate', e => {
 })
 
 self.addEventListener('fetch', e => {
-	console.log('serviceWorker fetching!')
-
 	e.respondWith(
 		fetch(e.request)
 		.then(res => {
